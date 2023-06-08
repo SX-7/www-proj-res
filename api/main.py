55c667f46937e052df605a0ac47a983e539c2baa
SX-7 <sn1.test.preria.2002@gmail.com>
2023-06-08 19:02:46 +0200

    Added db key removal endpoint

--- api/main.py
+++ api/main.py
@@ -5,11 +5,13 @@ app = Flask(__name__)
 
 @app.route('/api')
 def get_sentiments():
+    # iniialize db connection
     datastore_client = datastore.Client()
+    # setup and execute query
     kind = "Sentiment"
     query = datastore_client.query(kind=kind)
     data = list(query.fetch())
-    print("Returned query count is: " + str(len(data)))
+    # return query results to the user
     return jsonify([{
          "key_id":entity.id,
          "content":entity["content"],
@@ -19,26 +21,25 @@ def get_sentiments():
 
 @app.route('/api', methods=["POST"])
 def sample_analyze_sentiment():
-    # analyze content
+    # initialize language analyzer connection
     client = language_v1.LanguageServiceClient()
-
+    # get request info
     content = request.get_json()
-
+    # check for compliance with arbitrary API rules
     if "content" in content:
         content = content["content"]
     else:
         return '', 400
-    
+    # JIC
     if isinstance(content, bytes):
         content = content.decode("utf-8")
-
+    # analyze
     type_ = language_v1.Document.Type.PLAIN_TEXT
     document = {"type_": type_, "content": content}
-
     response = client.analyze_sentiment(request={"document": document})
     sentiment = response.document_sentiment
 
-    # Instantiates a client
+    # Instantiates a db client
     datastore_client = datastore.Client()
 
     # The kind for the new entity
@@ -54,6 +55,19 @@ def sample_analyze_sentiment():
 
     # Saves the entity
     datastore_client.put(entity)
+    return '', 204
+
+@app.route('/api', methods=['DELETE'])
+def delete_sentiment_entry():
+    record_id = request.get_json
+    if "record_id" in record_id:
+        record_id = record_id["record_id"]
+    else:
+        return '', 400
+    datastore_client = datastore.Client()
+    kind = "Sentiment"
+    entity_key = datastore_client.key(kind,record_id)
+    datastore_client.delete(entity_key)
     return '', 204
 
 if __name__ == '__main__':
